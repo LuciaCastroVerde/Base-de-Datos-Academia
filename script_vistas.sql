6c9ba8d6eb45772b44c5d168e637d0123ba82727
LuciaCastroVerde <144533249+LuciaCastroVerde@users.noreply.github.com>
2025-09-16 21:22:44 -0300

    script_vistas.sql

--- script_vistas.sql
+++ script_vistas.sql
@@ -1,4 +1,4 @@
-// Vista para obtener el total facturado por ventas anualmente
+-- Vista para obtener el total facturado por ventas anualmente
   
 CREATE OR REPLACE VIEW ventas_por_año AS
 (SELECT YEAR(fecha_factura), SUM(total_factura)
@@ -6,7 +6,7 @@ CREATE OR REPLACE VIEW ventas_por_año AS
 
 SELECT * FROM ventas_por_año;
 
-// Vista para obtener los datos de alumnos que estén matriculados en más de un curso
+-- Vista para obtener los datos de alumnos que estén matriculados en más de un curso
   
 CREATE OR REPLACE VIEW alumno_multi_matricula AS
 (SELECT id_alumno, apellido_alumno, nombre_alumno
@@ -16,7 +16,7 @@ CREATE OR REPLACE VIEW alumno_multi_matricula AS
 
 SELECT * FROM alumno_multi_matricula;
 
-// Vista para analizar la performance comercial de cada vendedor
+-- Vista para analizar la performance comercial de cada vendedor
 
 CREATE OR REPLACE VIEW ventas_acumuladas_vendedor AS
 (SELECT apellido_vendedor, nombre_vendedor, id_vendedor, SUM(total_factura)
@@ -27,7 +27,7 @@ CREATE OR REPLACE VIEW ventas_acumuladas_vendedor AS
 
 SELECT * FROM ventas_acumuladas_vendedor;
 
-// Vista para analizar la matriculación por curso durante el año 2024
+-- Vista para analizar la matriculación por curso durante el año 2024
 
 CREATE OR REPLACE VIEW matriculas_por_curso_2024 AS
 (SELECT id_curso, nombre_curso, COUNT(id_curso_matricula)
@@ -38,7 +38,7 @@ CREATE OR REPLACE VIEW matriculas_por_curso_2024 AS
 
 SELECT * FROM matriculas_por_curso_2024;
 
-// Vista para conocer cantidad de alumnos matriculados en cada modalidad de cursada por año
+-- Vista para conocer cantidad de alumnos matriculados en cada modalidad de cursada por año
 
 CREATE OR REPLACE VIEW modalidad_cursos_por_año AS
 (SELECT YEAR(fecha_matricula), modalidad_curso, COUNT(id_modalidad_matricula)
